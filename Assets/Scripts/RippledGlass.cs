2f38bb2724035c8b829119f98685ce07d2c2d789
simeonis <56325172+simeonis@users.noreply.github.com>
2021-08-29 19:30:20 -0400

    Reworked lighting in TestingRange

--- Assets/Scripts/RippledGlass.cs
+++ Assets/Scripts/RippledGlass.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class RippledGlass : MonoBehaviour
 {
     [SerializeField] private float rippleDuration;
-    [SerializeField] private float rippleStrength;
     private IEnumerator ripple;
-
     private Material material;
 
     void Start()
@@ -19,41 +17,24 @@ public class RippledGlass : MonoBehaviour
     {
         if (collider.gameObject.tag == "Player")
         {
-            Debug.Log(collider.gameObject.name + " entered Rippled Glass");
-
             Vector3 worldPostion = collider.ClosestPointOnBounds(transform.position);
-            Debug.Log("World Position: " + worldPostion);
             Vector3 localPostion = transform.InverseTransformPoint(worldPostion);
-            Debug.Log("Local Position: " + localPostion);
 
             material.SetVector("_Center", localPostion);
 
-            // if (ripple != null) StopCoroutine(ripple);
-            // StartCoroutine(ripple = Ripple());
+            if (ripple != null) StopCoroutine(ripple);
+            StartCoroutine(ripple = Ripple());
         }
     }
 
-    void OnTriggerExit(Collider collider)
+    private IEnumerator Ripple()
     {
-        if (collider.gameObject.tag == "Player")
+        float percent = 0.0f;
+        while (percent < 1.0f)
         {
-            Debug.Log(collider.gameObject.name + " exited Rippled Glass");
+            percent += Time.deltaTime / rippleDuration;
+            material.SetFloat("_Percent", percent);
+            yield return null;
         }
     }
-
-    // private IEnumerator Ripple()
-    // {
-    //     float current = rippleStrength;
-    //     float currentCount = 3;
-    //     float target = 0;
-
-    //     float percent = 0.0f;
-    //     while (percent < 1.0f)
-    //     {
-    //         percent += Time.deltaTime / rippleDuration;
-    //         material.SetFloat("_RippleCount", Mathf.Lerp(currentCount, target, percent));
-    //         material.SetFloat("_RippleStrength", Mathf.Lerp(current, target, percent));
-    //         yield return null;
-    //     }
-    // }
 }
